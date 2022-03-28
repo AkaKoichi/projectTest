053e642eb04e473764134079b36ba7e2164cf4cc
AkaKoichi <bruninhomartins240@gmail.com>
2022-03-28 08:33:00 +0100

    a

--- public/javascripts/requests/userRequests.js
+++ public/javascripts/requests/userRequests.js
@@ -44,3 +44,22 @@ async function logout() {
         console.log(err);
     }
 }
+
+async function register(player) {
+    try {
+        // TODO: Verify user information  and give errors
+        const response = await fetch("/api/users/register",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+              },
+            body: JSON.stringify(player) 
+        });
+        var  result= await response.json();
+        return {inserted: response.status==200 , result: result };
+    } catch (err) {
+        // Treat 500 errors here
+        console.log(err);
+    }
+} 
